ecc2fa2c5645bce1c35c26e2ec24a7bec24847f3
AlexAndriamahaleo <alex.andriamahaleo@etu.univ-amu.fr>
2017-12-29 13:08:56 +0100

    Update desgin, login form - registration form in progress

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -19,7 +19,6 @@ class DefaultController extends Controller
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
     }
-
     /**
      * @Route("/admin")
      */
